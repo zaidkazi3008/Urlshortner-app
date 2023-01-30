6505c9482430eb5c632e1b8600c48880d1886a45
zaidkazi3008 <zaidkazi.in@gmail.com>
2023-01-30 08:52:56 +0530

    project  Committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Footer from './components/footer/Footer';
+import LinkInput from './components/linkInput/LinkInput';
+import LinkResult from './components/linkResult/LinkResult';
+import Navbar from './components/navbar/Navbar';
+
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar/>
+      <main className='main'>
+      <LinkInput setInputValue={setInputValue}/>
+      <LinkResult inputValue={inputValue} />
+      </main>
+      <Footer/>
     </div>
   );
 }
